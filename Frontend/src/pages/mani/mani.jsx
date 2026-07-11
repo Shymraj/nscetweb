a9ebe967614535d25c7959c5f31dbda361810d50
manish <santhanammanish@gmail.com>
2026-07-11 21:37:22 +0530

    hi muthu

---
+++ Frontend/src/pages/mani/mani.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className="about-page-container">
+      <h1>About NSCET</h1>
+      <p>This is the new modern about page.</p>
+    </div>
+  );
+};
+
+export default About;
