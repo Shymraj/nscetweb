056dc15850e77243f941392e6041979cb5720100
Gowtham <gowthamangowthaman9714@gmail.com>
2026-08-20 09:52:46 +0530

    BLANK

---
+++ Frontend/src/pages/AboutUs/Developmentplan/components/InstitutionalStrength.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InstitutionalStrength = () => {
+  return (
+    <section className="institutional-strength">
+      {/* Placeholder for Institutional Strength */}
+    </section>
+  );
+};
+
+export default InstitutionalStrength;
---
+++ Frontend/src/pages/AboutUs/Developmentplan/components/KeyEnablers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const KeyEnablers = () => {
+  return (
+    <section className="key-enablers">
+      {/* Placeholder for Key Enablers */}
+    </section>
+  );
+};
+
+export default KeyEnablers;
